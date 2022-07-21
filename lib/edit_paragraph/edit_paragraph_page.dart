2ff40b9427718edb8f46964189d8017d1a531aea
i-b8o <blackview1617@gmail.com>
2022-07-21 20:58:12 +0300

    gdfdgf

--- lib/edit_paragraph/edit_paragraph_page.dart
+++ lib/edit_paragraph/edit_paragraph_page.dart
@@ -1,42 +1,37 @@
 import 'package:flutter/material.dart';
 
+import 'bottom_bar_black.dart';
+
 class EditParagraphPage extends StatelessWidget {
   const EditParagraphPage({Key? key, required this.text}) : super(key: key);
   final String text;
 
-  void _showModalBottomSheet(BuildContext context) {
-    showModalBottomSheet(
-        context: context,
-        builder: (context) {
-          return Container(
-            height: MediaQuery.of(context).size.height * 0.3,
-          );
-        });
-  }
-
   @override
   Widget build(BuildContext context) {
+    double _width = MediaQuery.of(context).size.width;
+    double _height = MediaQuery.of(context).size.height;
+    double _padding = 15;
     return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Icon(
-          Icons.mode_edit_outline,
-          color: Color(0xFFff9500),
-        ),
-        backgroundColor: Color(0xFFd4d5d5),
-
-      ),
       floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
-      body: GestureDetector(
-         onTap: () {
-                _showModalBottomSheet(context);
-              },
-        child: Container(
-            padding: EdgeInsets.only(
-              top: MediaQuery.of(context).padding.top + 15,
-            ),
-            child: Text(text)),
-      ),
+      body: Container(
+          height: double.infinity,
+          padding: EdgeInsets.only(
+            top: MediaQuery.of(context).padding.top + _padding,
+          ),
+          child: Stack(children: [
+            SelectableText(text),
+            BottomBarBlack(),
+            Positioned(
+                bottom: 0,
+                child: Container(
+                  decoration: BoxDecoration(
+                      borderRadius:
+                          BorderRadius.vertical(top: Radius.circular(30)),
+                      color: Colors.white),
+                  width: _width,
+                  height: _height * 0.24,
+                ))
+          ])),
     );
   }
 }
